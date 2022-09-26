f94c9a0f45feb2eb4c336a5cdf978d66d2ae8a41
Brian Berns <brianberns@gmail.com>
2022-09-26 11:19:46 -0400

    Modularized.

---
+++ AttoCompiler/Generator/Function.fs
@@ -0,0 +1,114 @@
+﻿namespace Atto.Generator
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+open type SyntaxFactory
+
+open Atto
+
+module Function =
+
+    let private objType =
+        PredefinedType(
+            Token(SyntaxKind.ObjectKeyword))
+
+    let private generateParameter (id : Parser.Identifier) =
+        Parameter(Identifier(id.String))
+            .WithType(objType)
+
+    let private generateParameterList ids =
+        let comma =
+            Token(SyntaxKind.CommaToken)
+                |> SyntaxNodeOrToken.op_Implicit
+        seq {
+            for i, id in Seq.indexed ids do
+                if i > 0 then comma
+                generateParameter id
+                    |> SyntaxNodeOrToken.op_Implicit
+        }
+            |> SeparatedList
+            |> ParameterList
+
+    let rec private generateExpr expr : Syntax.ExpressionSyntax =
+        match expr with
+            | Parser.Literal (Parser.Num num) ->
+                LiteralExpression(
+                    SyntaxKind.NumericLiteralExpression,
+                    Literal(num))
+            | Parser.Literal (Parser.Str str) ->
+                LiteralExpression(
+                    SyntaxKind.StringLiteralExpression,
+                    Literal(str))
+            | Parser.Name id ->
+                IdentifierName(id.String)
+            | Parser.Call (fnName, args) ->
+                InvocationExpression(
+                    IdentifierName(fnName.String))
+                    .WithArgumentList(generateArgumentList args)
+            | Parser.Equal (left, right) ->
+                InvocationExpression(
+                    MemberAccessExpression(
+                        SyntaxKind.SimpleMemberAccessExpression,
+                        MemberAccessExpression(
+                            SyntaxKind.SimpleMemberAccessExpression,
+                            IdentifierName("System"),
+                            IdentifierName("Object")),
+                        IdentifierName("Equals")))
+                    .WithArgumentList(
+                        generateArgumentList [|left; right|])
+            | Parser.Operation (op, left, right) ->
+                let opKind =
+                    match op with
+                        | Parser.Addition -> SyntaxKind.AddExpression
+                        | Parser.Subtraction -> SyntaxKind.SubtractExpression
+                        | Parser.Multiplication -> SyntaxKind.MultiplyExpression
+                let cast node =
+                    CastExpression(
+                        PredefinedType(
+                            Token(SyntaxKind.DoubleKeyword)),
+                        node)
+                BinaryExpression(
+                    opKind,
+                    generateExpr left |> cast,
+                    generateExpr right |> cast)
+            | Parser.If (pred, ifTrue, ifFalse) ->
+                ConditionalExpression(
+                    generateExpr pred,
+                    generateExpr ifTrue,
+                    generateExpr ifFalse)
+            | Parser.Print expr ->
+                InvocationExpression(
+                    IdentifierName("Print"))
+                    .WithArgumentList(
+                        generateArgumentList [| expr |])
+
+    and private generateArgument expr =
+        Argument(generateExpr expr)
+
+    and private generateArgumentList exprs =
+        let comma =
+            Token(SyntaxKind.CommaToken)
+                |> SyntaxNodeOrToken.op_Implicit
+        seq {
+            for i, expr in Seq.indexed exprs do
+                if i > 0 then comma
+                generateArgument expr
+                    |> SyntaxNodeOrToken.op_Implicit
+        }
+            |> SeparatedList
+            |> ArgumentList
+
+    let generate (fn : Parser.Function) =
+        MethodDeclaration(
+            returnType = objType,
+            identifier = fn.Name.String)
+            .AddModifiers(
+                Token(SyntaxKind.StaticKeyword))
+            .WithParameterList(
+                generateParameterList fn.Args)
+            .WithExpressionBody(
+                generateExpr fn.Expr
+                    |> ArrowExpressionClause)
+            .WithSemicolonToken(
+                Token(SyntaxKind.SemicolonToken))
+            :> Syntax.MemberDeclarationSyntax
---
+++ AttoCompiler/Generator/Program.fs
@@ -0,0 +1,108 @@
+﻿namespace Atto.Generator
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+open type SyntaxFactory
+
+open Basic.Reference.Assemblies
+
+module Program =
+
+    let private mainMethod =
+
+        let stmt =
+            ExpressionStatement(
+                InvocationExpression(
+                    IdentifierName("main")))
+
+        MethodDeclaration(
+            returnType = PredefinedType(
+                Token(
+                    SyntaxKind.VoidKeyword)),
+            identifier = "Main")
+            .AddModifiers(
+                Token(SyntaxKind.StaticKeyword))
+            .AddBodyStatements(stmt)
+
+    let private printMethod =
+        MethodDeclaration(
+            PredefinedType(
+                Token(SyntaxKind.ObjectKeyword)),
+            SyntaxFactory.Identifier("Print"))
+            .WithModifiers(
+                TokenList(
+                    Token(SyntaxKind.StaticKeyword)))
+            .WithParameterList(
+                ParameterList(
+                    SingletonSeparatedList(
+                        Parameter(
+                            SyntaxFactory.Identifier("value"))
+                            .WithType(
+                                PredefinedType(
+                                    Token(SyntaxKind.ObjectKeyword))))))
+            .WithBody(
+                Block(
+                    ExpressionStatement(
+                        InvocationExpression(
+                            MemberAccessExpression(
+                                SyntaxKind.SimpleMemberAccessExpression,
+                                MemberAccessExpression(
+                                    SyntaxKind.SimpleMemberAccessExpression,
+                                    IdentifierName("System"),
+                                    IdentifierName("Console")),
+                                IdentifierName("WriteLine")))
+                            .WithArgumentList(
+                                ArgumentList(
+                                    SingletonSeparatedList(
+                                        Argument(
+                                            IdentifierName("value")))))),
+                    ReturnStatement(
+                        IdentifierName("value"))))
+
+    let private generateFunctions fns =
+        [|
+            for fn in fns do
+                Function.generate fn
+        |]
+
+    let generate (assemblyName : string) fns =
+
+        let classNode =
+            let members = generateFunctions fns
+            ClassDeclaration($"{assemblyName}Type")
+                .AddMembers(mainMethod, printMethod)
+                .AddMembers(items = members)
+
+        let namespaceNode =
+            NamespaceDeclaration(
+                IdentifierName(assemblyName))
+                .AddMembers(classNode)
+
+        let compilationUnit =
+            CompilationUnit().AddMembers(namespaceNode)
+        printfn "%A" <| compilationUnit.NormalizeWhitespace()
+
+        let compilation =
+            let references : MetadataReference[] =
+                [|
+                    Net60.mscorlib
+                    Net60.SystemRuntime
+                    Net60.SystemConsole
+                |]
+            let options =
+                CSharpCompilationOptions(OutputKind.ConsoleApplication)
+                    .WithMainTypeName($"{namespaceNode.Name}.{classNode.Identifier}")
+            CSharpCompilation
+                .Create(assemblyName)
+                .AddSyntaxTrees(compilationUnit.SyntaxTree)
+                .AddReferences(references)
+                .WithOptions(options)
+        let result = compilation.Emit($"{assemblyName}.dll")
+        for diagnostic in result.Diagnostics do
+            printfn "%A" diagnostic
+        if result.Success then printfn "Success"
+
+        System.IO.File.Copy(
+            "App.runtimeconfig.json",
+            $"{assemblyName}.runtimeconfig.json",
+            overwrite = true)
